3529036b5cfadf0d0c608e8abfe2a7866c1665fd
Luis Sandoval <luis.sandovaldlg@gmail.com>
2021-12-04 13:07:14 -0600

    Modifiying cacheMatrix to return the expected solution
    Adding documented testing procedure

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a list containing get,set,getinv,setinv in order to be able
+## to cache the inverse of a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invcache <- NULL
+    set <- function(y){
+      x <<- y
+      invCache <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) invcache <<- inv
+    getinv <- function() invcache
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix, used in conjunction with makeCacheMatrix
+## Returns the cached inverse if it exists, or else it computes it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x' 
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("Getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
